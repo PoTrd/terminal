4cc71fd2eab82be51e730fa8a57ca1c42de60781
PoTrd <paul.taurand@gmail.com>
2025-08-31 13:38:26 +0200

    Initial commit with files

---
+++ wezterm.lua
@@ -0,0 +1,74 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Nord (Gogh)",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font_size = 12.5,
+
+	background = {
+		{
+			source = {
+				File = "/Users/paultaurand/Pictures/bg_monterey.jpg",
+			},
+			hsb = {
+				hue = 1.0,
+				saturation = 1.02,
+				brightness = 0.25,
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.90,
+		},
+		{
+			source = {
+				Color = "#282c35",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.55,
+		},
+	},
+	window_padding = {
+		left = 3,
+		right = 3,
+		top = 0,
+		bottom = 0,
+	},
+}
+
+return config
+
+-- local wezterm = require("wezterm")
+
+-- local config = wezterm.config_builder()
+
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font" , {weight="Regular"})
+-- config.font_size = 17
+
+-- config.enable_tab_bar = false
+
+-- config.window_decorations = "TITLE | RESIZE"
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 10
+
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#011423",
+-- 	cursor_bg = "#47FF9C",
+-- 	cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
+
+-- return config
+
