98ee0ebdacd76c8254ab0ba4379a4ceb463c315e
PoTrd <paul.taurand@gmail.com>
2025-08-31 13:40:53 +0200

    wip# update Wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -15,7 +15,7 @@ config = {
 	background = {
 		{
 			source = {
-				File = "/Users/paultaurand/Pictures/bg_monterey.jpg",
+				File = "<image_path>/bg_monterey.jpg",
 			},
 			hsb = {
 				hue = 1.0,
@@ -44,31 +44,3 @@ config = {
 }
 
 return config
-
--- local wezterm = require("wezterm")
-
--- local config = wezterm.config_builder()
-
--- config.font = wezterm.font("CaskaydiaCove Nerd Font" , {weight="Regular"})
--- config.font_size = 17
-
--- config.enable_tab_bar = false
-
--- config.window_decorations = "TITLE | RESIZE"
--- config.window_background_opacity = 0.8
--- config.macos_window_background_blur = 10
-
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	cursor_bg = "#47FF9C",
--- 	cursor_border = "#47FF9C",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
-
--- return config
-
